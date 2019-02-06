0875a9da60942e81840a490072ba66d077d04946
Sterling Walsh <sterling.walshdev@gmail.com>
2019-02-06 15:02:04 -0500

    client server comms

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors')
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users:[{
@@ -21,6 +24,11 @@ const database = {
         password: '4',
         entries: 0,
         joined: new Date()
+    }],
+    logins:[{
+        id:'123',
+        name: 'John',
+        hash: ''
     }]
 }
 
@@ -32,16 +40,17 @@ app.post('/signin', (req,res) => {
     console.log(req.body);
     req.body.email === database.users[0].email 
         && req.body.password === database.users[0].password
-        ?   res.json('success')
+        ?   res.json(database.users[0])
         :   res.status(400).json('faiure')
     
 });
 
 app.post('/register', (req, res) => {
     const {email, name, password} = req.body;
-
+    console.log("server Post", req.body);
+    let hashedPW = '';
     bcrypt.hash(password, null, null, (err, hash) => {
-        console.log(hash);
+        hashedPW = hash;
     });
 
     database.users.push({
@@ -52,29 +61,34 @@ app.post('/register', (req, res) => {
         entries: 0,
         joined: new Date()
     });
-    res.json(`welcome ${database.users[database.users.length - 1].name}`);
+    database.logins.push({
+        id:'125',
+        name: name,
+        hash: hashedPW
+    });
+    res.json(database.users[database.users.length - 1]);
 });
 
 app.get('/profile/:id', (req,res) => {
     const {id} = req.params;
-    const user = verifyUser(id);
+    const user = verifyUserID(id);
     user ? res.json(user) : res.status(404).json('User not found');
 });
 
 app.put('/image', (req,res) => {
     const {id} = req.body;
-    const user = verifyUser(id);
+    const user = verifyUserID(id);
     if(user){
-        return res.json(++user.entries)
+        console.log("submit", user);
+        return res.json({entries:++user.entries});
     }else{
         return res.status(400).json('User not found');
     }
 });
 
-const verifyUser = (id) => {
+const verifyUserID = (id) => {
     let found = false;
     database.users.forEach(user => {
-        console.log(typeof user.id, user.id, typeof id, id);
         if(user.id === id) {
             console.log(true);
             found = user;
@@ -83,7 +97,7 @@ const verifyUser = (id) => {
     return found;
 }
 
-const port = 3000;
+const port = 3001;
 app.listen(port, () => {
     console.log("server ready on", port)
 });
