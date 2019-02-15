c41ef2052090c60f315d63390cd5b22d8f43ed15
Sterling Walsh <sterling.walshdev@gmail.com>
2019-02-15 10:31:49 -0500

    dynamic port

--- server.js
+++ server.js
@@ -34,8 +34,7 @@ app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
 app.put('/image', (req, res) => image.handleImage(req, res, db));
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
-const port = 3001;
-app.listen(port, () => {
-    console.log("server ready on", port)
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`server ready on ${process.env.PORT}`)
 });
 
